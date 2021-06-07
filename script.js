24035a6c056a950750eac0f2700f51bfd5bea2f5
Cristina Lorenzo <cristina@numericco.com>
2021-06-07 17:08:57 +0200

    toolbox controls

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const clearEl = document.getElementById('clear');
 
 const ctx = canvas.getContext('2d');
 
-let size = 20;
+let size = 10;
 let isPressed = false;
 let color = 'black';
 let x
@@ -57,5 +57,30 @@ function drawLine(x1, y1, x2, y2) {
     ctx.stroke();
 }
 
+function updateSizeOnScreen() {
+    sizeEl.innerText = size;
+}
 
-colorEl.addEventListener('change', (e) => color = e.target.value)
+increaseBtn.addEventListener('click', () => {
+    size += 5;
+
+    if(size > 50) {
+        size = 50;
+    }
+
+    updateSizeOnScreen();
+})
+
+decreaseBtn.addEventListener('click', () => {
+    size -= 5;
+
+    if(size < 5) {
+        size = 5;
+    }
+
+    updateSizeOnScreen();
+})
+
+colorEl.addEventListener('change', (e) => color = e.target.value);
+
+clearEl.addEventListener('click', () =>ctx.clearRect(0,0, canvas.width, canvas.height));
